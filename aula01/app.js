ac112d76f6c793f89a77c881655474b3da0586d4
Joel Santos <joeloliveira56@gmail.com>
2026-01-14 21:34:04 -0300

    feat: adicionando novos exemplos da api e respostas get

--- aula01/app.js
+++ aula01/app.js
@@ -3,19 +3,47 @@ const app = express(); //cria a const app que herda as funções do express
 const port = 3000; // A porta que aplicação/API ira rodar;
 
 
-app.get("/",(req, res) =>{
+//rota com resposta do tipo texto 
+app.get("/", (req, res) => {
 
     res.send("Hello World!!!");
 
 })
-
-app.get("/teste",(req, res) =>{
-
+//nova rota com resposta do tipo texto
+app.get("/teste", (req, res) => {
     res.send("Hello World meu fi!!!");
-
 })
 
-app.listen(port, ()=>{
+//rota com resposta do tipo JSON
+app.get('/usuario', (req, res) => {
+    res.json({
+        nome: "João",
+        idade: 25,
+        cidade: "São Paulo"
+    });
+});
+
+//rota get com status HTTP - status 200 (sucesso)
+app.get('/status', (req, res) => {
+    res.status(200).send("Tudo OK!");
+});
+
+//rota get com status HTTP - status 500 (erro interno do servidor)
+app.get('/erro', (req, res) => {
+    res.status(500).json({
+        erro: "Erro interno do servidor"
+    })
+});
+
+app.get("/usuario/:id", (req, res)=>{
+    const {id} = req.params;
+    res.status(200).json({
+        id: id,
+        nome: "User exemplo"
+    })
+})
+
+app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`);
 })
 
