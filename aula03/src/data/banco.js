d6f9b6fd7756ad66255a1e62438960b6adf5e537
Joel Santos <joeloliveira56@gmail.com>
2026-01-19 20:03:52 -0300

    feat: initialize aula03 project with Express and Nodemon
    - Add package.json with dependencies for Express and Nodemon
    - Create in-memory database for users in banco.js
    - Implement UsuarioModel with CRUD operations for user management

---
+++ aula03/src/data/banco.js
@@ -0,0 +1,15 @@
+//Banco de dados em memória
+export let usuarios = [
+    {
+        id:1, 
+        nome: "Amanda", 
+        email: "amanda@email.com", 
+        telefone: "(84) 9090-9090"
+    },
+    {
+        id:2, 
+        nome: "Joel", 
+        email: "joel@email.com", 
+        telefone: "(84) 4002-8922"
+    }
+]
---
+++ aula03/src/models/usuario/UsuarioModel.js
@@ -0,0 +1,42 @@
+import { usuarios } from "../../data/banco.js";
+
+export class UsuarioModel{
+
+    static listarTodos(){
+        return usuarios;
+    }
+    static buscarPorId(id){
+        return usuarios.find(u => u.id === parseInt(id));
+    }
+    static criarUsuario(nome, email, telefone){
+        const novoUsuario = {
+            id: usuarios.length + 1,
+            nome: nome,
+            email: email,
+            telefone: telefone
+        };
+        usuarios.push(novoUsuario);
+        return novoUsuario;
+    }
+    static atualizarUsuario(id, nome, email, telefone){
+        const index = usuarios.findIndex(u => u.id === parseInt(id));
+        usuarios[index] = {
+            id: parseInt(id),
+            nome: nome,
+            email: email,
+            telefone: telefone
+        }
+        return usuarios[index];
+    }
+    static deletarUsuario(id){
+         const index = usuarios.findIndex(u => u.id === parseInt(id));
+         if(index===-1){
+            return false
+         }
+         usuarios.splice(index, 1);
+         return true;
+    }
+
+
+
+}
